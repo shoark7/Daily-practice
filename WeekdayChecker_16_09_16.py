f20af2ff9bfd0f387512a9d0aef332df6d0faf05
Sunghwan Park <shoark7@naver.com>
2016-09-17 10:07:26 +0900

    Get the logical flow of WeekdayCehcker

--- WeekdayChecker_16_09_16.py
+++ WeekdayChecker_16_09_16.py
@@ -17,8 +17,12 @@ def weekday_checker(year, month, day):
         else:
             return False
 
-    days_of_month = [31, 28, 31, 30, 31, 30,31,31,30, 31,30,31] # Jan to Feb
+    days_counter = 0
     weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    days_of_month = [31, 28, 31, 30, 31, 30,31,31,30, 31,30,31] # Jan to Feb
+    if is_lunatic(year):
+        days_of_month[1] = 29
+    # If year is lunatic, days of Feb changes to 29.
 
     # Input type and value test.
     if any(map(lambda x: type(x) != type(int(3)), [year,month,day])):
@@ -32,6 +36,15 @@ def weekday_checker(year, month, day):
                          str(days_of_month[month -1] + 1 + (1 if is_lunatic(year) and month == 2 else 0)) +\
                          " with given month.")
         # This statement would be difficult. It considers lunatic years so day range would change.
+
+    """
+    Now we have to count days. If given date is 2004/03/02,
+    1. we count year days from 1 to 2003
+    2. we count month days from Jan to Feb(right before March
+    3. we count given day - 1  because day of start date is 1.
+    4. finally we get the remainder of days_counter divided by 7.
+    5. We get the day.
+    """
 
 
 
