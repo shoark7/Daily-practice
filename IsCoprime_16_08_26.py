297428a9dec4e5c3e14291a7ab467247dc45d27a
Sunghwan Park <shoark7@gmail.com>
2016-10-03 17:43:55 +0900

    Complete MemoizeFibo_16_10_03

--- IsCoprime_16_08_26.py
+++ IsCoprime_16_08_26.py
@@ -22,7 +22,11 @@ def is_coprime(*two_value):
         first_value = two_value[0]
         second_value = two_value[1]
 
-        for i in range(2, min(two_value) + 1):
+        for i in range(2, min(two_value) + 1):    while True:
+        a, b = randint(2, 10), randint(2,10) # 두 개의 랜덤 정수 각각 할당
+        if a == b: continue
+        break
+
             if first_value % i == 0 and second_value % i == 0:
                 return False
 
@@ -33,12 +37,9 @@ def is_coprime(*two_value):
 # 올바른 값을 출력하는지 10번 정도 검증
 
 for i in range(10):
-    while True:
-        a, b = randint(2, 10), randint(2,10) # 두 개의 랜덤 정수 각각 할당
-        if a == b: continue
-        break
 
     if is_coprime(a,b):
         print('{:4d}, {:4d} are coprime'.format(a,b))
     else:
         print('{:4d}, {:4d} are not'.format(a,b))
+
