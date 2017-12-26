e3bad155e78cac55b5afb53683d8b6cb6a39e9c8
Sunghwan Park <shoark7@gmail.com>
2017-12-26 13:44:55 +0900

    Add input validation

--- all-python/make_acronym_171226.py
+++ all-python/make_acronym_171226.py
@@ -12,6 +12,7 @@ This module supports several options for acronyms
 Start Date: 2017/12/26
 End Date  : 2017/12/26
 """
+from collections import Sequence
 
 
 def generate_acronym(sentence, all_caps=True, ignore_words=[], glue=''):
@@ -22,11 +23,20 @@ def generate_acronym(sentence, all_caps=True, ignore_words=[], glue=''):
         all_caps: Whether returned acronym is all capitalized. Defaults to True
         ignore_words: A sequence of str that's to be ignored in the returned string.
                       Defaults to None.
+                      If ignore_words is a single str instance,
+                      any single letter of it will be ignored.
         glue: A seperator to concatenate first letters of words. Defaults to ''
 
     :return:
         A str instance.
     """
+    if not isinstance(sentence, str):
+        raise TypeError("Sentence must be a str instance")
+    if ignore_words and not isinstance(ignore_words, Sequence):
+        raise TypeError("ignore_words must be a Sequence instance")
+    if glue and not isinstance(glue, str):
+        raise TypeError("glue must be a str instance")
+
     words = sentence.split()
 
     # Maybe in a more readable way
@@ -42,5 +52,5 @@ def generate_acronym(sentence, all_caps=True, ignore_words=[], glue=''):
 
 
 print(generate_acronym('World Health organization', False))
-print(generate_acronym('With us, the us', False, ['the']))
+print(generate_acronym('With us, the us', False, 'the'))
 print(generate_acronym('With us, the us', False))
